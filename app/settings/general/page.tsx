e7839776ff192d5778c33df484b4dad0d9b58531
chellal aziz <chellalaziz31@gmail.com>
2024-03-01 20:21:26 +0100

    creation of the repo

---
+++ app/settings/general/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>general</div>;
+};
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        sidebar: {
+          active: "#6b32ec",
+          background: "#f5f5ff",
+          iconColor: "#636c80",
+        },
       },
     },
   },
