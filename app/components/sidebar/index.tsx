b971cbedb721617f135d41d868fc85d6d538a4bd
chellal aziz <chellalaziz31@gmail.com>
2024-03-02 16:52:23 +0100

    add links

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,9 +1,38 @@
 import React from "react";
+import {
+  LucideIcon,
+  LayoutDashboard,
+  BadgeDollarSign,
+  CircleUserRound,
+  Settings,
+  WalletCards,
+} from "lucide-react";
+import path from "path";
+interface IsSidebarItem {
+  name: string;
+  icon: LucideIcon;
+  path: string;
+  item?: IsSubItem[];
+}
+interface IsSubItem {
+  name: string;
+  path: string;
+}
 
+const items: IsSidebarItem[] = [
+  {
+    name: "Dashboard",
+    icon: LayoutDashboard,
+    path: "/",
+  },
+];
 const Sidebar = () => {
   return (
-    <div className=" fixed top-0 left-0 h-screen w-64 bg-white shadow-lg z-10">
-      Sidebar
+    <div className=" fixed top-0 left-0 h-screen w-64 bg-white shadow-lg z-10 p-4">
+      <div className="flex flex-col space-y-10 w-full ">
+        <img src="/images/logo.png" alt="logo" className="h-10 w-fill" />
+        <div>sidebar items</div>
+      </div>
     </div>
   );
 };
